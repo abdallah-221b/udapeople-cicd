0903aa58c9cddc4a2f2811924de3aaa4a445b666
MohamedSheh7ata <mohammedalqttan@gmail.com>
2025-04-27 05:28:53 +0300

    config infra 4

--- frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
+++ frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
@@ -1,3 +1,23 @@
+// import * as React from 'react';
+// import { shallow } from 'enzyme';
+// import { LoadingMessage } from 'app/components/LoadingMessage';
+
+// describe('<LoadingMessage>', () => {
+//   describe('Props', () => {
+//     describe('message', () => {
+//       it('Should render the props message', () => {
+//         const message = 'Hello!';
+//         const wrapper = shallow(<LoadingMessage message={message} />);
+//         // expect(wrapper.contains(<span>{message}</span>)).toBeTruthy();
+//         expect(wrapper.find('.loading-message-text').text()).toEqual(message);
+//       });
+//     });
+//   });
+// });
+// function expect(arg0: any) {
+//   throw new Error('Function not implemented.');
+// }
+
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import { LoadingMessage } from 'app/components/LoadingMessage';
@@ -8,13 +28,9 @@ describe('<LoadingMessage>', () => {
       it('Should render the props message', () => {
         const message = 'Hello!';
         const wrapper = shallow(<LoadingMessage message={message} />);
-        // expect(wrapper.contains(<span>{message}</span>)).toBeTruthy();
-        expect(wrapper.find('.loading-message-text').text()).toEqual(message);
+        expect(wrapper.contains(<span>{message}</span>)).toBeTruthy();
       });
     });
   });
 });
-function expect(arg0: any) {
-  throw new Error('Function not implemented.');
-}
 
